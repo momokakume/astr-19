5c0e4b7ad5a1e13f6fe78afcf8301a6fcba0b03e
momokakume <mkume@ucsc.edu>
2026-04-08 11:00:41 -0700

    Add files via upload

---
+++ code_journal1_1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print('Name: Momoka Kume')
+print('Pronouns: She/Her')
+print('My favorite movie is the Disney movie "Descendants" because I am a fan of the actress who played the main character in the movie.')
+print('My favorite food is mochi because I can eat it in many different ways.')
