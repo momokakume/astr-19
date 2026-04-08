c68059bc76a5589529adec5fe92cbba96c0b8c61
momokakume <mkume@ucsc.edu>
2026-04-08 11:17:14 -0700

    Add files via upload

---
+++ code_journal1_2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 11:04:58 2026
+
+@author: momokakume
+"""
+
+x = 2.8
+y = 3.7
+
+print("x =", x)
+print("y =", y)
+
+print("x+y =", x+y)
+print(type(x+y))
+
+
+x = 9
+y = 5
+
+print("x =", x)
+print("y =", y)
+
+print("x-y =", x-y)
+print(type(x-y)) 
+
+
+x = 5.8
+y = 3
+
+print("x =", x)
+print("y =", y)
+
+print("x*y =", x*y)
+print(type(x*y))     
